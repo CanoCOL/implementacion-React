903f6d3c5c98da076d2b65aba337eb36db92163c
Miguel Cano <>
2022-10-13 11:29:16 -0500

    Hola

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Cerrar } from "./Components/Cerrar/Cerrar";
+import { Inicio } from "./Components/Inicio/Inicio";
+import { NavBar } from "./Components/NavBar/NavBar";
+import { VistaPrincipal } from "./Components/VistaPrincipal/VistaPrincipal";
+
+
+export function App() {
+    return (
+        <>
+            <div className="si">
+                <Inicio />
+
+                {/* <Cerrar /> */}
+
+                {/* <VistaPrincipal /> */}
+            </div>
+        </>
+    )
+}
